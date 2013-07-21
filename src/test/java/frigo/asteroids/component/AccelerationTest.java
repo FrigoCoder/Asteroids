70bfa707051b862628195f1bd62524e035bd98d0
Frigo <none@none>
2013-07-21 16:04:46 +0200

    - removed unused method

--- src/test/java/frigo/asteroids/component/AccelerationTest.java
+++ src/test/java/frigo/asteroids/component/AccelerationTest.java
@@ -11,11 +11,6 @@ public class AccelerationTest {
     private Acceleration acceleration = new Acceleration(2.0, 3.0);
 
     @Test
-    public void add_implicit_vector () {
-        assertThat(acceleration.add(1.0, 1.0), is(new Acceleration(3.0, 4.0)));
-    }
-
-    @Test
     public void add_explicit_vector () {
         assertThat(acceleration.add(new Vector(1.0, 1.0)), is(new Acceleration(3.0, 4.0)));
     }
