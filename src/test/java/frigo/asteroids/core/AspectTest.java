c5ba102ccef3e9023d1c1025c4c20f886ffdd19b
Frigo <none@none>
2013-08-10 03:26:23 +0200

    - added unit tests for Aspect.none

--- src/test/java/frigo/asteroids/core/AspectTest.java
+++ src/test/java/frigo/asteroids/core/AspectTest.java
@@ -4,7 +4,6 @@ package frigo.asteroids.core;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import frigo.asteroids.component.Position;
@@ -14,33 +13,43 @@ import frigo.asteroids.component.Velocity;
 public class AspectTest {
 
     private World world = new World();
-    private Entity entity = world.createEntity();
-    private Aspect aspect = Aspect.allOf(Position.class, Velocity.class);
+    private Aspect all = Aspect.allOf(Position.class, Velocity.class);
+    private Aspect none = Aspect.noneOf(Position.class, Velocity.class);
 
     @Test
     public void aspect_all_matches_entities_having_all_components () {
-        world.set(entity, new Position(1, 1));
-        world.set(entity, new Velocity(0.1, 0.1));
-        assertThat(world.matches(entity, aspect), is(true));
+        Entity entity = world.createEntity(new Position(0, 0), new Velocity(0, 0));
+        assertThat(world.matches(entity, all), is(true));
     }
 
     @Test
     public void aspect_all_does_not_match_entities_not_having_all_components () {
-        world.set(entity, new Position(1, 1));
-        assertThat(world.matches(entity, aspect), is(false));
+        Entity entity = world.createEntity(new Position(0, 0));
+        assertThat(world.matches(entity, all), is(false));
     }
 
     @Test
     public void aspect_all_matches_entities_having_more_components () {
-        world.set(entity, new Position(1, 1));
-        world.set(entity, new Velocity(0.1, 0.1));
-        world.set(entity, new Renderable(1.0));
-        assertThat(world.matches(entity, aspect), is(true));
+        Entity entity = world.createEntity(new Position(0, 0), new Velocity(0, 0), new Renderable(0));
+        assertThat(world.matches(entity, all), is(true));
     }
 
-    @Ignore
     @Test
-    public void test_none_matcher () {
+    public void aspect_none_matches_entities_having_no_such_components () {
+        Entity entity = world.createEntity();
+        assertThat(world.matches(entity, none), is(true));
+    }
+
+    @Test
+    public void aspect_none_does_not_match_entities_having_such_components () {
+        Entity entity = world.createEntity(new Position(0, 0));
+        assertThat(world.matches(entity, none), is(false));
+    }
+
+    @Test
+    public void aspect_none_matches_entities_having_other_components () {
+        Entity entity = world.createEntity(new Renderable(0));
+        assertThat(world.matches(entity, none), is(true));
     }
 
 }
