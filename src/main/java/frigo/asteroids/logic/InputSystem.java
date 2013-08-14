fc779ea3a7a16458bf0e3ae6a17f02da49e32f6d
Frigo <none@none>
2013-08-14 22:14:17 +0200

    - made InputSystem more flexible, preparing for angular rotation

--- src/main/java/frigo/asteroids/logic/InputSystem.java
+++ src/main/java/frigo/asteroids/logic/InputSystem.java
@@ -1,6 +1,9 @@
 
 package frigo.asteroids.logic;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.jogamp.newt.event.KeyEvent;
 
 import frigo.asteroids.component.Acceleration;
@@ -11,6 +14,7 @@ import frigo.asteroids.core.Entity;
 import frigo.asteroids.core.Logic;
 import frigo.asteroids.core.World;
 import frigo.asteroids.message.KeyHeld;
+import frigo.asteroids.message.KeyMessage;
 import frigo.asteroids.message.KeyPressed;
 
 public class InputSystem implements Logic {
@@ -26,29 +30,31 @@ public class InputSystem implements Logic {
     public void update (World world, double elapsedSeconds) {
         Entity entity = world.getEntitiesFor(controllableAspect).iterator().next();
         PlayerControllable controllable = world.get(entity, PlayerControllable.class);
-        double thrust = controllable.thrust;
-        Acceleration acceleration = world.get(entity, Acceleration.class);
-        for( KeyPressed event : world.getMessages(KeyPressed.class) ){
-            acceleration = acceleration.add(getDirection(event.key).mul(thrust));
-        }
-        for( KeyHeld event : world.getMessages(KeyHeld.class) ){
-            acceleration = acceleration.add(getDirection(event.key).mul(thrust));
-        }
-        world.set(entity, acceleration);
-    }
 
-    private Vector getDirection (short key) {
-        switch( key ){
-            case KeyEvent.VK_UP:
-                return new Vector(0, 1);
-            case KeyEvent.VK_DOWN:
-                return new Vector(0, -1);
-            case KeyEvent.VK_LEFT:
-                return new Vector(-1, 0);
-            case KeyEvent.VK_RIGHT:
-                return new Vector(1, 0);
-            default:
-                return new Vector(0, 0);
+        double thrust = controllable.thrust;
+        double rotation = controllable.rotation;
+
+        List<KeyMessage> messages = new LinkedList<>();
+        messages.addAll(world.getMessages(KeyPressed.class));
+        messages.addAll(world.getMessages(KeyHeld.class));
+
+        for( KeyMessage message : messages ){
+            switch( message.key ){
+                case KeyEvent.VK_UP:
+                    world.set(entity, world.get(entity, Acceleration.class).add(new Vector(0, thrust)));
+                    break;
+                case KeyEvent.VK_DOWN:
+                    world.set(entity, world.get(entity, Acceleration.class).add(new Vector(0, -thrust)));
+                    break;
+                case KeyEvent.VK_LEFT:
+                    world.set(entity, world.get(entity, Acceleration.class).add(new Vector(-thrust, 0)));
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    world.set(entity, world.get(entity, Acceleration.class).add(new Vector(thrust, 0)));
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
