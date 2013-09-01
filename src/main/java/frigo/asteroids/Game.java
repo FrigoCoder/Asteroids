c83eda50870abb33e1729d5f1efe622bee5cdb56
Frigo <none@none>
2013-09-01 19:47:03 +0200

    - added XStream and a dumper method

--- src/main/java/frigo/asteroids/Game.java
+++ src/main/java/frigo/asteroids/Game.java
@@ -1,15 +1,31 @@
 
 package frigo.asteroids;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver;
+
 import frigo.asteroids.core.World;
 import frigo.asteroids.jogl.JOGLRunner;
 
 public class Game {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
         World world = new AsteroidsWorldFactory().createWorld();
+
+        // dump(world, "world.json");
+
         JOGLRunner runner = new JOGLRunner(world, 1024, 768, 100);
         runner.start();
+    }
+
+    private static void dump (World world, String filename) throws IOException {
+        XStream xstream = new XStream(new JsonHierarchicalStreamDriver());
+        try( FileWriter writer = new FileWriter(filename) ){
+            xstream.toXML(world, writer);
+        }
     }
 
 }
