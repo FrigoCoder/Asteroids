ec44ebb0c56608371c1ff4f46a25eabacd90de5b
Frigo <none@none>
2015-04-02 14:45:49 +0200

    - transitioned to maven - removed applet because it is a terrible solution

---
+++ src/main/scripts/asteroids.bat
@@ -0,0 +1,16 @@
+setlocal
+
+cd %~dp0
+
+set JAVA=java.exe
+if defined JAVA_HOME set JAVA=%JAVA_HOME%\bin\java.exe
+
+set JVM_OPTIONS=-XX:+UseG1GC -XX:MaxGCPauseMillis=10 -Dsun.java2d.opengl=true -Dsun.java2d.d3d=false
+
+set CLASSPATH=lib/*
+
+set MAIN_CLASS=frigo.asteroids.Game
+
+%JAVA% %JVM_OPTIONS% -cp %CLASSPATH% %MAIN_CLASS%
+
+endlocal
